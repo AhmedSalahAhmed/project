9e6bb8dee6cbd66c2c046b972b0f00da347dc7c1
siddighope <noory4433@gmail.com>
2022-03-16 20:37:17 +0200

    fix currency update bug on central

--- HardCurrency/resources/views/centralbank/currency.blade.php
+++ HardCurrency/resources/views/centralbank/currency.blade.php
@@ -123,20 +123,17 @@
 
 
 
-                                            <form action="{{ route('currency.update', $currency->id) }}" method="POST">
-
+                                            <form id="editForm" method="post" action="{{ route('currency.update', $currency->id) }}" >
                                                 @csrf
-
                                                 @method('put')
 
-                                                <input type="text" name="currency_name" class="form-control mb-3" placeholder=" العملة " value="{{$currency->currency_name}}" disabled="disabled">
-                                                <input type="text" name="abbreviation" class="form-control mb-3" placeholder="سعر الشراء " value="{{$currency->abbreviation}}">
-                                                <input type="text" name="symbol" class="form-control mb-3" placeholder="سعر البيع " value="{{$currency->symbol}}">
 
+                                                <input id="currency_name" type="text" name="currency_name" class="form-control mb-3" placeholder=" العملة " value="{{$currency->currency_name}}" disabled="disabled"/>
+                                                <input id="abbreviation" type="text" name="abbreviation" class="form-control mb-3" placeholder="سعر الشراء " value="{{$currency->abbreviation}}"/>
+                                                <input id="symbol" type="text" name="symbol" class="form-control mb-3" placeholder="سعر البيع " value="{{$currency->symbol}}"/>
+                                                <input id="_token" type="hidden" value="{{ csrf_token() }}"/>
 
-
-
-                                                <button class="btn btn-twitter float-end px-5" type="submit">تم</button>
+                                                <input onclick="submitForm('{{$currency->id}}', event)" class="btn btn-twitter float-end px-5" value="تم" type="submit"/>
 
                                             </form>
                                             @include('sweetalert::alert')
@@ -155,5 +152,49 @@
                     {{ $currencies->links() }}
                 </div>
 
+                <script>
+                    const submitForm = (id, e) => {
+
+                        console.log(id)
+
+                        e.preventDefault()
+
+                        const data = {
+                            abbreviation: document.getElementById("abbreviation").value,
+                            symbol: document.getElementById("symbol").value,
+                            currency_name: document.getElementById("currency_name").value,
+                            _token: document.getElementById("_token").value
+                        }
+
+                        const formData = new FormData()
+
+                        formData.append("abbreviation", data.abbreviation)
+                        formData.append("symbol", data.symbol)
+                        formData.append("currency_name", data.currency_name)
+                        formData.append("_token", data._token)
+                        console.log(id)
+                        console.log(data)
+                        $.ajax({
+                            type:"post",
+                            url:"currency/"+ id + "?_method=put",
+                            data: formData,
+                            contentType: false,
+                            processData: false,
+                            cache: false,
+                            dataType: 'json',
+                            success:(data) => {
+                                console.log("data*************")
+                                console.log(data)
+                                location.replace("{{route('currency.index')}}")
+                            },
+                            error:(error)=>{
+                                location.replace("{{route('currency.index')}}")
+                                console.log(error.responseJSON)
+                            }
+                        })
+                    }
+
+
+                </script>
 
 @endsection
