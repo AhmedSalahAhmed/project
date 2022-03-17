47640759f034c44413c22ae4d671945a2d116525
AhmedSalahAhmed <wad.alshaikh98@gmail.com>
2022-03-17 16:27:02 +0200

    multi select bug fixed

--- HardCurrency/resources/views/centralbank/layouts/app.blade.php
+++ HardCurrency/resources/views/centralbank/layouts/app.blade.php
@@ -191,18 +191,15 @@
                 copyTagClasses: false,
             });
         });
-        $("#state").onChange(function() {
+        $("#state").change(function() {
         console.log("h");
-
-
         if ($(this).data('options') === undefined) {
-
             /*Taking an array of all options-2 and kind of embedding it on the select1*/
             $(this).data('options', $('#locale option').clone());
         }
         var id = $(this).val();
         var options = $(this).data('options').filter('[value=' + id + ']');
-        $('#select2').html(options);
+        $('#locale').html(options);
     });
     </script>
 
